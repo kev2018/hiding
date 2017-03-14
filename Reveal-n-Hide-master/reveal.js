ac2f74de65339bd4fd72f9c0458351ebf5f371b3
kev2018 <8kludwig@stpatrick.org>
2017-03-14 13:58:46 -0500

    almost

--- Reveal-n-Hide-master/reveal.js
+++ Reveal-n-Hide-master/reveal.js
@@ -1,31 +1,42 @@
-function toggle(button){
+/*function toggle(button){
     let articles = document.getElementsByTagName("article");
+    let div = document.getElementsByClassName("grey");
+    let selected2 = div[button-1];
     let selected = articles[button-1];
-    /*if(selected.style.display === "none"){
-        reveal(selected);
-        console.log("reveal "+button);
-    }*/
-    if(selected.style.z === 1){
+    if(selected.style.display === "none"){
         reveal(selected);
         console.log("reveal "+button);
     }
+    if(selected.style.zIndex === "1"){
+        reveal(selected2);
+        console.log("reveal "+button);
+    }
     else{
-        hide(selected);
+        hide(selected2);
         console.log("hide "+button);
     }
 
 }
 
-/*function reveal(art){
+function reveal(art){
     art.style.display = "block";
 }
 
 function hide(art){
     art.style.display = "none";
-}*/
-function reveal(art){
-    art.style.z = 0;
+}
+function reveal(div){
+    div.style.zIndex = "0";
 }
 
-function hide(art){
-    art.style.z = 1;
+function hide(div){
+    div.style.zIndex = "1";
+*/
+function toggle(){
+  if(document.getElementsByClassName("grey").style.zIndex == "1"){
+   document.getElementsByClassName("grey").style.zIndex = "-1";
+   }
+   else{
+    document.getElementsByClassName("grey").style.zIndex = "1";
+   }
+}
